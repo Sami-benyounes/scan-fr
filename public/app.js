61b34fad596a10d2dde3ce1503ba06e35bd52d74
MathieuElisabeth <mathieuregina75@gmail.com>
2021-01-21 22:09:05 +0100

    (feature/front) : adding connexion page

--- public/app.js
+++ public/app.js
@@ -1,4 +1,7 @@
 import "./pages/mangas/styles/mangas.scss";
+import "./pages/planning/styles/planning.scss";
+import "./pages/connexion/styles/connexion.scss";
+import "./pages/nous_rejoindre/styles/nous_rejoindre.scss";
 import './images/Logo.png';
 import './images/ace-novel.png';
 import './images/list.png';
