4fc925e30553017589547c68692d88d7eef225a9
MathieuElisabeth <75541968+MathieuElisabeth@users.noreply.github.com>
2021-01-27 16:45:10 +0100

    (feature/front) : adding footer (#8)
    * (feature/front) : adding footer
    
    * (chore/front) : change footer to partials

--- public/app.js
+++ public/app.js
@@ -3,6 +3,7 @@ import "./pages/planning/styles/planning.scss";
 import "./pages/connexion/styles/connexion.scss";
 import "./pages/nous_rejoindre/styles/nous_rejoindre.scss";
 import "./pages/acceuil/styles/acceuil.scss";
+import "./pages/partials/footer.scss";
 import './images/Logo.png';
 import './images/ace-novel.png';
 import './images/list.png';
